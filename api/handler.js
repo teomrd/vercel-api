51011027b53615518aedecc23f5f5ad430715de9
Theo Mironidis <mironidis.gr@gmail.com>
2023-01-26 15:23:53 +0000

    add handler

--- api/handler.js
+++ api/handler.js
@@ -1,4 +1,4 @@
-export default function handler(request, response) {
+ export default function handler(request, response) {
   response.status(200).json({
     body: request.body,
     query: request.query,
